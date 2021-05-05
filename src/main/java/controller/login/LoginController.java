037675db315799c3c18c61346a5d5fba00cb8e0e
Raluca <ralucatrandafir08@gmail.com>
2021-05-05 20:14:24 +0300

    Cancel and Login Button
    Set text to login button. Activate cancel button.

--- src/main/java/controller/login/LoginController.java
+++ src/main/java/controller/login/LoginController.java
@@ -2,22 +2,52 @@ package controller.login;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-import java.sql.SQLOutput;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+//import java.sql.SQLOutput;
+
+public class LoginController  {
     @FXML
     private Button loginButton;
     @FXML
     private Button cancelButton;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private Label loginMessageLabel;
 
     public void loginButtonOnAction(ActionEvent actionEvent) {
         System.out.println("Login Button On Action!");
-        System.out.println("Still do to !");
+        loginMessageLabel.setText("You tried to login!");
+
     }
 
     public void cancelButtonOnAction(ActionEvent actionEvent) {
         System.out.println("Cancel Button On Action!");
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
+
+   // @Override
+    /*public void initialize(URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("resources/images/sigla.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+
+        File lockFile = new File("resources/images/lacat.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+    }*/
 }
