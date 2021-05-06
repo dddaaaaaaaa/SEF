353d10884fb734ed41a9cd78f02e5522e9fab2ea
Raluca <ralucatrandafir08@gmail.com>
2021-05-06 15:36:56 +0300

    Data Base Connection on Server
    RMA-25 Database connection on online server

--- src/main/java/domain/DatabaseConnection.java
+++ src/main/java/domain/DatabaseConnection.java
@@ -1,18 +1,12 @@
 package domain;
 import java.sql.*;
 
-public class DatabaseConnection {
+public class DatabaseConnection extends DatabaseCredentials{
     public Connection databaseLink;
 
     public Connection getConnection(){
-        String databaseName="remindme"; //personal database name needed
-        String databaseUser="root"; //personal database user needed
-        String databasePassword="raluca123"; // personal database password needed
-        String url="jdbc:mysql://localhost:3306/"+ databaseName; // check your local host url
-
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            databaseLink = DriverManager.getConnection(url,databaseUser, databasePassword);
+            databaseLink = DriverManager.getConnection(super.url,super.databaseName, super.databasePassword);
 
         }catch(Exception e){
             e.printStackTrace();
