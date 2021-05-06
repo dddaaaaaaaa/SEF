0ed979b77449dddad7d3eddfb212f987c9b9ab27
Raluca <ralucatrandafir08@gmail.com>
2021-05-06 22:43:55 +0300

    RMA-6 Registration GUI
    RMA-6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    /*private Stage stage;
+
+    public void changeStage(String fxml) throws IOException{ // function to change the scene
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stage.getScene().setRoot(pane);
+    }*/
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/login.fxml"));
+        primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.DECORATED);
         primaryStage.setScene(new Scene(root,520,400));
         primaryStage.show();
