3a434e395f6829b969c7d1a12469eb1b4a3028bf
Raluca <ralucatrandafir08@gmail.com>
2021-05-06 14:11:02 +0300

    Database Connection Comments
    RMA-5

--- src/main/java/domain/DatabaseConnection.java
+++ src/main/java/domain/DatabaseConnection.java
@@ -5,10 +5,10 @@ public class DatabaseConnection {
     public Connection databaseLink;
 
     public Connection getConnection(){
-        String databaseName="remindme";
-        String databaseUser="root";
-        String databasePassword="raluca123";
-        String url="jdbc:mysql://localhost:3306/"+ databaseName;
+        String databaseName="remindme"; //personal database name needed
+        String databaseUser="root"; //personal database user needed
+        String databasePassword="raluca123"; // personal database password needed
+        String url="jdbc:mysql://localhost:3306/"+ databaseName; // check your local host url
 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
