5266fbf59699246702aa4acbd5d41e6543b4dc2b
dddaaaaaaaa <vlad.ciuleanu@student.upt.ro>
2021-05-09 22:30:19 +0300

    Added temp button to spawn new event dialog

---
+++ src/main/java/controller/event/AddEventController.java
@@ -0,0 +1,9 @@
+package controller.event;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AddEventController {
+
+}
--- src/main/java/controller/user/UserViewController.java
+++ src/main/java/controller/user/UserViewController.java
@@ -4,8 +4,10 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
@@ -14,6 +16,17 @@ import java.util.ResourceBundle;
 public class UserViewController implements Initializable {
     @FXML
     private ListView<String> UserListView;
+
+    @FXML
+    private Button createNewButton;
+
+    public void createNewButtonOnAction(ActionEvent actionEvent)
+    {
+        //quick debug print
+        System.out.println("DEBUG (uview controller line 25) - button press, adding new event");
+
+        //spawn new scene
+    }
 
     ObservableList<String> List = FXCollections.observableArrayList();
 
