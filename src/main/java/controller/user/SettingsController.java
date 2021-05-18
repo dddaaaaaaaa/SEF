55b792a69b9f8d316706ccd274faffa5ce729785
dddaaaaaaaa <vlad.ciuleanu@student.upt.ro>
2021-05-18 14:47:13 +0300

    RMA-8 Upgrade implemented

--- src/main/java/controller/user/SettingsController.java
+++ src/main/java/controller/user/SettingsController.java
@@ -10,8 +10,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.time.LocalDate;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+
+import static javafx.scene.paint.Color.color;
 
 
 public class SettingsController extends UserViewInterface implements Initializable {
@@ -23,18 +28,16 @@ public class SettingsController extends UserViewInterface implements Initializab
     private User currentUser;
     @FXML
     private Label MandatoryLabel, UserTypeLabel;
-    private EventOrganizerUser eventUser;
-    private BasicUser basicUser;
+    //private EventOrganizerUser eventUser;
+    //private BasicUser basicUser;
     @FXML
     private Button UpgradeButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         currentUser = UserHolder.getInstance().getUser();
-        //System.out.println("This is settings user " + currentUser.getUsername());
+        MandatoryLabel.setTextFill(color(1, 0, 0));
         InitializeSettingsFields();
-
-
     }
 
     public void InitializeSettingsFields() {
@@ -42,50 +45,113 @@ public class SettingsController extends UserViewInterface implements Initializab
         LastNameTextField.setText(currentUser.getLastName());
         EmailTextField.setText(currentUser.getEmail());
         UserTypeLabel.setText(currentUser.getUser());
-
-        if (currentUser.getUser().equals("Event Organizer User")) {
-            eventUser = (EventOrganizerUser) currentUser;
-            if (AddressTextField.getText().isBlank() || PhoneTextField.getText().isBlank() || BirthDate.getValue() == null || OccupationTextField.getText().isBlank()) {
-                MandatoryLabel.setText("Mandatory fields!");
-                UpgradeButton.setVisible(false);
-            }
-
-            else {
-                //set fields from database
-                /*AddressTextField.setText(eventUser.getAddress());
-                //System.out.println("\n"+ eventUser.getBirth());
-                //BirthDate.setValue(eventUser.getBirth());
-                OccupationTextField.setText(eventUser.getOccupation());
-                PhoneTextField.setText(eventUser.getPhone());
-
-                 */
-            }
-        }
-        if(currentUser.getUser().equals("Basic User")){
-            MandatoryLabel.setText("");
-            UpgradeButton.setVisible(true);
-        }
-
+        AddressTextField.setText(currentUser.getAddress());
+        PhoneTextField.setText(currentUser.getPhone());
+        BirthDate.setValue(currentUser.getBirth());
+        if (currentUser instanceof BasicUser)
+            UpgradeButton.setDisable(false);
     }
 
     public void updateCredentialsButtonOnAction(ActionEvent actionEvent) {
-        if (AddressTextField.getText().isBlank() || PhoneTextField.getText().isBlank() || OccupationTextField.getText().isBlank())
-            MandatoryLabel.setText("Mandatory fields!");
-        else {
-            String address = AddressTextField.getText();
-            String phone = PhoneTextField.getText();
-            String occupation = OccupationTextField.getText();
-            LocalDate date = BirthDate.getValue();
-            eventUser.setAddress(address);
-            //eventUser.setBirth(BirthDate.getValue());
-            //System.out.println(BirthDate.getValue());
-            eventUser.setOccupation(occupation);
-            eventUser.setPhone(phone);
+        String phone = PhoneTextField.getText();
 
+        if(phone.length() > 12 || !phone.matches("[0-9]*"))
+        {
+            MandatoryLabel.setText("Invalid phone input!");
+            return;
+        }
+
+        if(EmailTextField.getText().isBlank())
+        {
+            MandatoryLabel.setText("Email can't be blank!");
+            return;
+        }
+
+        currentUser.setFirstName(FirstNameTextField.getText());
+        currentUser.setLastName(LastNameTextField.getText());
+        currentUser.setAddress(AddressTextField.getText());
+        currentUser.setEmail(EmailTextField.getText());
+        currentUser.setPhone(phone);
+        if(BirthDate.getValue() != null)
+            currentUser.setBirth(BirthDate.getValue());
+
+        try
+        {
+            Connection connectDB = new DatabaseConnection().getConnection();
+            PreparedStatement ps = connectDB.prepareStatement("UPDATE \"user\" SET \"firstName\" = ?, " +
+                            "\"lastName\" = ?, \"email\" = ?, \"address\" = ?, \"phone\" = ?, \"birth\" = ? " +
+                            "WHERE username = ?;");
+            ps.setString(1, currentUser.getFirstName());
+            ps.setString(2, currentUser.getLastName());
+            ps.setString(3, currentUser.getEmail());
+            ps.setString(4, currentUser.getAddress());
+            ps.setString(5, currentUser.getPhone());
+            ps.setString(6, currentUser.getBirth().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+            ps.setString(7, currentUser.getUsername());
+
+            ps.executeUpdate();
+
+            MandatoryLabel.setTextFill(color(0, 1, 0));
+            MandatoryLabel.setText("Credential update successful!");
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            e.getCause();
+            MandatoryLabel.setText("SQL Error! Unable to update credentials!");
         }
     }
 
     public void UpgradeButtonOnAction(ActionEvent actionEvent) {
+        if (FirstNameTextField.getText().isBlank() || LastNameTextField.getText().isBlank() ||
+                AddressTextField.getText().isBlank() || PhoneTextField.getText().isBlank() ||
+                BirthDate.getValue() == null) {
+            MandatoryLabel.setText("Full name, address, phone number and birthdate are required for upgrade!");
+        }
+        else {
+            String phone = PhoneTextField.getText();
 
+            if(phone.length() > 12 || !phone.matches("[0-9]*"))
+            {
+                MandatoryLabel.setText("Invalid phone input!");
+                return;
+            }
+
+            currentUser.setFirstName(FirstNameTextField.getText());
+            currentUser.setLastName(LastNameTextField.getText());
+            currentUser.setAddress(AddressTextField.getText());
+            currentUser.setEmail(EmailTextField.getText());
+            currentUser.setPhone(phone);
+            currentUser.setBirth(BirthDate.getValue());
+            //String occupation = OccupationTextField.getText();
+
+            try
+            {
+                Connection connectDB = new DatabaseConnection().getConnection();
+                PreparedStatement ps = connectDB.prepareStatement("UPDATE \"user\" SET \"firstName\" = ?, " +
+                        "\"lastName\" = ?, \"email\" = ?, \"type\" = ?, \"address\" = ?, \"phone\" = ?, \"birth\" = ? " +
+                        "WHERE username = ?;");
+                ps.setString(1, currentUser.getFirstName());
+                ps.setString(2, currentUser.getLastName());
+                ps.setString(3, currentUser.getEmail());
+                ps.setString(4, "Event Organizer User");
+                ps.setString(5, currentUser.getAddress());
+                ps.setString(6, currentUser.getPhone());
+                ps.setString(7, currentUser.getBirth().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+                ps.setString(8, currentUser.getUsername());
+
+                ps.executeUpdate();
+
+                MandatoryLabel.setTextFill(color(0, 1, 0));
+                MandatoryLabel.setText("Upgrade successful! Relog to see changes!");
+                UpgradeButton.setDisable(true);
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+                e.getCause();
+                MandatoryLabel.setText("SQL Error! Unable to update credentials!");
+            }
+        }
     }
 }
